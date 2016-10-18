b82a4b468b4ae2b6c4c14f77ba3fdbb34a13dea2
Matt Frowe <frowem@ucles.internal>
2016-10-18 11:19:22 +0100

    added styling and form validation

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'home-page',
-    templateUrl: 'home.component.html'
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
     constructor() { }
--- app/wine/wine-form/wine-form.component.ts
+++ app/wine/wine-form/wine-form.component.ts
@@ -5,13 +5,13 @@ import { Wine } from './../../shared/models/wine.model';
 @Component({
     moduleId: module.id,
     //selector: 'wine-form',
-    templateUrl: 'wine-form.component.html',
-    styleUrls: ['wine-form.component.css']
+    templateUrl: 'wine-form.component.html'
+    //styleUrls: ['wine-form.component.css']
 })
 export class WineFormComponent implements OnInit {
 
     colours:string[];
-    newWine: Wine = new Wine();
+    newWine: Wine = new Wine(0,'','','',0,'','')
 
     constructor(private _service: WineService) { }
 
@@ -22,4 +22,6 @@ export class WineFormComponent implements OnInit {
     onSubmit(){
         console.log(this.newWine);
     }
+
+    get diagnostic() { return JSON.stringify(this.newWine); }
 }
